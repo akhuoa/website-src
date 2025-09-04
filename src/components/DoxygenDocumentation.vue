5faa9c2d16834328cdc05e17b894dae6ca7287ea
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2025-09-04 19:41:59 +1200

    Update base URL

--- src/components/DoxygenDocumentation.vue
+++ src/components/DoxygenDocumentation.vue
@@ -1,11 +1,17 @@
 <template>
 <div class="doxygendocs">
-    <doxygen-xml :baseURL="'/documentation/api/' + props.version" />
+    <doxygen-xml :baseURL="baseURL" />
 </div>
 </template>
 
 <script setup>
 import { DoxygenXml } from 'vue3-doxygen-xml'
+import { computed } from 'vue'
+
 const props = defineProps(['version'])
 
+const baseURL = computed(() => {
+  const basePath = import.meta.env.BASE_URL
+  return `${basePath}documentation/api/${props.version}`
+})
 </script>
