e0c796493ce210b3ea1aafcabe6febc5787eb41c
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2025-09-07 21:15:03 +1200

    Remove padding for bigger container on mobile

--- src/views/APIDocumentationView.vue
+++ src/views/APIDocumentationView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bg-gray-50 py-12">
-    <div class="max-w-4xl mx-auto px-4">
+    <div class="max-w-4xl mx-auto">
       <h1 class="text-4xl font-bold text-center mb-12 bg-gradient-to-r from-blue-600 to-indigo-600 bg-clip-text text-transparent">
         API Documentation
       </h1>
